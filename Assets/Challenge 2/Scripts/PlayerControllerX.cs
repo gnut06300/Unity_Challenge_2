ae053ee5fcfe6725f2f46fc07a30acbd0c48e9e5
gnut06300 <gnut@gnut.eu>
2023-01-11 09:58:13 +0100

    Lessons 8 and 9

--- Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -6,13 +6,27 @@ public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
 
+    private float waitTime = 1.0f;
+    private float timer = 0.0f;
+    //  private float visualTime = 0.0f;
+
     // Update is called once per frame
     void Update()
     {
+        timer += Time.deltaTime;
+
+        // Check if we have reached beyond 2 seconds.
+        // Subtracting two is more accurate over time than resetting to zero.
+        
         // On spacebar press, send dog
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            if (timer > waitTime)
+            {
+                // Remove the recorded 1 seconds.
+                timer = timer - waitTime;
+                Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            }
         }
     }
 }
